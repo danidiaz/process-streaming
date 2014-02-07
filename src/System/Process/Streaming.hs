6d65d9c0f48638b3e19f11ea2df1e109743d6ab3
Daniel Diaz <diaz.carrete@facebook.com>
2014-02-07 18:20:21 +0100

    consume and feed

--- src/System/Process/Streaming.hs
+++ src/System/Process/Streaming.hs
@@ -3,19 +3,21 @@
 
 module System.Process.Streaming ( 
         ConcurrentlyE (..),
+        consume,
+        feed,
         createProcessE,
         shellPiped,
         procPiped,
-        noNothingHandles,
-        IOExceptionHandler,
-        StreamSifter,
-        fromConsumer,
-        consume,
+--        noNothingHandles,
+--        IOExceptionHandler,
+--        StreamSifter,
+--        fromConsumer,
+--        consume,
 --        StdCombinedConsumer,
 --        combined,
 --        consumeCombined,
-        feed,
-        terminateOnError        
+--        feed,
+--        terminateOnError        
     ) where
 
 import Data.Maybe
@@ -68,17 +70,56 @@ instance (Show e, Typeable e) => Alternative (ConcurrentlyE e) where
     ConcurrentlyE $ either id id <$> race as bs
 
 --
-consumez :: Handle -> 
-            (IOException -> e) ->
-            Parser a IO (Either e a) ->
-            IO (Either e a) 
-consumez = undefined
-           
+mailbox2Handle :: Input ByteString -> Handle -> IO ()
+mailbox2Handle mailbox handle = 
+     finally (runEffect $ fromInput mailbox >-> toHandle handle)
+             (hClose handle) 
 
---
+handle2Mailbox :: Handle -> Output ByteString -> IO ()
+handle2Mailbox handle mailbox = 
+     finally (runEffect $ fromHandle handle >-> toOutput mailbox)
+             (hClose handle) 
+
+consumeMailbox :: Input z -> (Producer z IO () -> IO (Either e a)) -> IO (Either e a)
+consumeMailbox inMailbox consumer = do
+    result <- consumer $ fromInput inMailbox
+    case result of 
+        Left e -> return $ Left e
+        Right r -> do
+            runEffect $ fromInput inMailbox >-> P.drain 
+            return $ result
+
+feedMailbox :: (Consumer z IO () -> IO (Either e a)) -> Output z -> IO (Either e a)
+feedMailbox feeder outMailbox = feeder $ toOutput outMailbox
+
+try' :: (IOException -> e) -> IO (Either e a) -> IO (Either e a)
+try' handler action = try action >>= either (return . Left . handler) return
+
+consume :: (IOException -> e) 
+        -> Handle 
+        -> (Producer ByteString IO () -> IO (Either e a))
+        -> IO (Either e a) 
+consume exHandler h c = try' exHandler $ do 
+    (outbox, inbox, seal) <- spawn' Unbounded
+    (_,r) <- concurrently  (do a <- async $ handle2Mailbox h outbox
+                               wait a `finally` atomically seal)
+                           (consumeMailbox inbox c) 
+    return r                                
+
+feed :: (IOException -> e)
+     -> Handle 
+     -> (Consumer ByteString IO () -> IO (Either e a))
+     -> IO (Either e a) 
+feed exHandler h c = try' exHandler $ do 
+    (outbox, inbox, seal) <- spawn' Unbounded
+    (r,_) <- concurrently (do a <- async $ feedMailbox c outbox
+                              wait a `finally` atomically seal) 
+                          (mailbox2Handle inbox h)
+    return r
+
 createProcessE :: CreateProcess 
-               -> ErrorT IOException IO (Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle)
-createProcessE = ErrorT . try . createProcess
+               -> IO (Either IOException (Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle))
+createProcessE = try . createProcess
 
 shellPiped :: String -> CreateProcess 
 shellPiped cmd = (shell cmd) { std_in = CreatePipe, 
@@ -102,84 +143,70 @@ noNothingHandles (mb_stdin_hdl, mb_stdout_hdl, mb_stderr_hdl, ph) =
                  <*> mb_stderr_hdl 
                  <*> pure ph)
 
-type IOExceptionHandler e = IOException -> e
-
-type StreamSifter e a = Producer ByteString IO () -> ErrorT e IO a
-
-fromConsumer :: (Monoid w, Error e) => Consumer ByteString (WriterT w (ErrorT e IO)) () -> StreamSifter e w
-fromConsumer consumer producer = runEffect . execWriterP $ hoist (lift.lift) producer >-> consumer
-
-try' :: IOExceptionHandler e -> IO (Either e a) -> IO (Either e a)
-try' handler action = try action >>= either (return . Left . handler) return
-
-writeToMailbox :: Handle -> (ByteString -> a) -> Output a -> IO ()
-writeToMailbox handle mapper mailbox = 
-     finally (runEffect $ fromHandle handle >-> P.map mapper >-> toOutput mailbox)
-             (hClose handle) 
-
-consumeMailbox :: Input z -> (Producer z IO () -> ErrorT e IO a) -> IO (Either e a)
-consumeMailbox inMailbox consumer = do
-    result <- runErrorT . consumer $ fromInput inMailbox
-    case result of 
-        Left e -> return $ Left e
-        Right r -> do
-            runEffect $ fromInput inMailbox >-> P.drain 
-            return $ result
-
-consume' :: StreamSifter e a
-         -> StreamSifter e b
-         -> IOExceptionHandler e
-         -> (Handle, Handle) 
-         -> ErrorT e IO (a,b)
-consume' stdoutConsumer stderrConsumer exHandler (stdout_hdl, stderr_hdl) = ErrorT $ try' exHandler $ do 
-    (inMailbox1, outMailbox1, seal1) <- spawn' Unbounded
-    a1 <- async $ writeToMailbox stdout_hdl id inMailbox1
-    a2 <- async $ wait a1 `finally` atomically seal1 
-    a3 <- async $ consumeMailbox outMailbox1 stdoutConsumer 
-    (inMailbox2, outMailbox2, seal2) <- spawn' Unbounded
-    b1 <- async $ writeToMailbox stderr_hdl id inMailbox2
-    b2 <- async $ wait b1 `finally` atomically seal2 
-    b3 <- async $ consumeMailbox outMailbox2 stderrConsumer 
-    (_,r) <- waitAny [fmap Right a3,fmap Left b3]
-    -- is waiting here a problem???
-    flip finally (waitBoth a2 b2) $ case r of
-        Left rb -> case rb of 
-            Left e -> do
-                    cancel a3
-                    return $ Left e
-            Right b -> do 
-                ra <- wait a3
-                case ra of
-                    Left e -> return $ Left e -- drop b result
-                    Right a -> return $ Right (a,b)
-        Right ra -> case ra of 
-            Left e -> do
-                    cancel b3
-                    return $ Left e
-            Right a -> do
-                rb <- wait b3
-                case rb of 
-                    Left e -> return $ Left e -- drop a result
-                    Right b -> return $ Right (a,b)
-
-consume :: StreamSifter e a
-        -> StreamSifter e b
-        -> IOExceptionHandler e
-        -> (u,Handle, Handle,v)
-        -> (u,ErrorT e IO (a,b),v)
-consume stdoutReader stderrReader exHandler (u, stdout_hdl, stderr_hdl, v) =
-    (u, consume' stdoutReader stderrReader exHandler (stdout_hdl, stderr_hdl), v)
-
-type StdCombinedConsumer e a = Producer (Either ByteString ByteString) IO () -> ErrorT e IO a
-
-
-consumeCombined' :: Pipe ByteString ByteString IO X  
-                 -> Pipe ByteString ByteString IO X   
-                 -> StreamSifter e a
-                 -> IOExceptionHandler e
-                 -> (Handle, Handle) 
-                 -> ErrorT e IO a
-consumeCombined' = undefined
+--type IOExceptionHandler e = IOException -> e
+--
+--type StreamSifter e a = Producer ByteString IO () -> ErrorT e IO a
+--
+--fromConsumer :: (Monoid w, Error e) => Consumer ByteString (WriterT w (ErrorT e IO)) () -> StreamSifter e w
+--fromConsumer consumer producer = runEffect . execWriterP $ hoist (lift.lift) producer >-> consumer
+--
+--
+--
+--
+--consume' :: StreamSifter e a
+--         -> StreamSifter e b
+--         -> IOExceptionHandler e
+--         -> (Handle, Handle) 
+--         -> ErrorT e IO (a,b)
+--consume' stdoutConsumer stderrConsumer exHandler (stdout_hdl, stderr_hdl) = ErrorT $ try' exHandler $ do 
+--    (inMailbox1, outMailbox1, seal1) <- spawn' Unbounded
+--    a1 <- async $ writeToMailbox stdout_hdl id inMailbox1
+--    a2 <- async $ wait a1 `finally` atomically seal1 
+--    a3 <- async $ consumeMailbox outMailbox1 stdoutConsumer 
+--    (inMailbox2, outMailbox2, seal2) <- spawn' Unbounded
+--    b1 <- async $ writeToMailbox stderr_hdl id inMailbox2
+--    b2 <- async $ wait b1 `finally` atomically seal2 
+--    b3 <- async $ consumeMailbox outMailbox2 stderrConsumer 
+--    (_,r) <- waitAny [fmap Right a3,fmap Left b3]
+--    -- is waiting here a problem???
+--    flip finally (waitBoth a2 b2) $ case r of
+--        Left rb -> case rb of 
+--            Left e -> do
+--                    cancel a3
+--                    return $ Left e
+--            Right b -> do 
+--                ra <- wait a3
+--                case ra of
+--                    Left e -> return $ Left e -- drop b result
+--                    Right a -> return $ Right (a,b)
+--        Right ra -> case ra of 
+--            Left e -> do
+--                    cancel b3
+--                    return $ Left e
+--            Right a -> do
+--                rb <- wait b3
+--                case rb of 
+--                    Left e -> return $ Left e -- drop a result
+--                    Right b -> return $ Right (a,b)
+--
+--consume :: StreamSifter e a
+--        -> StreamSifter e b
+--        -> IOExceptionHandler e
+--        -> (u,Handle, Handle,v)
+--        -> (u,ErrorT e IO (a,b),v)
+--consume stdoutReader stderrReader exHandler (u, stdout_hdl, stderr_hdl, v) =
+--    (u, consume' stdoutReader stderrReader exHandler (stdout_hdl, stderr_hdl), v)
+--
+--type StdCombinedConsumer e a = Producer (Either ByteString ByteString) IO () -> ErrorT e IO a
+--
+--
+--consumeCombined' :: Pipe ByteString ByteString IO X  
+--                 -> Pipe ByteString ByteString IO X   
+--                 -> StreamSifter e a
+--                 -> IOExceptionHandler e
+--                 -> (Handle, Handle) 
+--                 -> ErrorT e IO a
+--consumeCombined' = undefined
 
 ---- Useful in combination with "bifold" of the "bifunctors" package.
 --combined :: (Either ByteString ByteString -> ByteString) 
@@ -211,43 +238,43 @@ consumeCombined' = undefined
 --consumeCombined combinedReader exHandler (u, stdout_hdl, stderr_hdl, v) =
 --    (u, consumeCombined' combinedReader exHandler (stdout_hdl, stderr_hdl), v)
 
-feed' :: Producer ByteString IO a
-      -> IOExceptionHandler e
-      -> Handle
-      -> ErrorT e IO b
-      -> ErrorT e IO b 
-feed' producer exHandler stdin_hdl action = ErrorT $ try' exHandler $ do
-    a1 <- async $ runEffect $ producer >-> toHandle stdin_hdl       
-    a2 <- async $ runErrorT action
-    r <- wait a2  
-    case r of
-        Left e -> cancel a1 >> (return $ Left e)
-        Right b -> wait a1 >> (return $ Right b)
-
-feed :: Producer ByteString IO a
-     -> IOExceptionHandler e
-     -> (Handle,ErrorT e IO b,v)
-     -> (ErrorT e IO b,v)
-feed producer exHandler (stdin_hdl,action,v) =
-    (feed' producer exHandler stdin_hdl action, v)
-
-terminateOnError :: (ErrorT e IO a,ProcessHandle)
-                 -> ErrorT e IO (a,ExitCode)
-terminateOnError (action,pHandle) = ErrorT $ do
-    result <- runErrorT action
-    case result of
-        Left e -> do    
-            terminateProcess pHandle  
-            return $ Left e
-        Right r -> do 
-            exitCode <- waitForProcess pHandle 
-            return $ Right (r,exitCode)  
-
-example1 =  terminateOnError 
-          . feed undefined undefined     
-          . consume undefined undefined undefined 
-          . noNothingHandles
-
+--feed' :: Producer ByteString IO a
+--      -> IOExceptionHandler e
+--      -> Handle
+--      -> ErrorT e IO b
+--      -> ErrorT e IO b 
+--feed' producer exHandler stdin_hdl action = ErrorT $ try' exHandler $ do
+--    a1 <- async $ runEffect $ producer >-> toHandle stdin_hdl       
+--    a2 <- async $ runErrorT action
+--    r <- wait a2  
+--    case r of
+--        Left e -> cancel a1 >> (return $ Left e)
+--        Right b -> wait a1 >> (return $ Right b)
+--
+--feed :: Producer ByteString IO a
+--     -> IOExceptionHandler e
+--     -> (Handle,ErrorT e IO b,v)
+--     -> (ErrorT e IO b,v)
+--feed producer exHandler (stdin_hdl,action,v) =
+--    (feed' producer exHandler stdin_hdl action, v)
+--
+--terminateOnError :: (ErrorT e IO a,ProcessHandle)
+--                 -> ErrorT e IO (a,ExitCode)
+--terminateOnError (action,pHandle) = ErrorT $ do
+--    result <- runErrorT action
+--    case result of
+--        Left e -> do    
+--            terminateProcess pHandle  
+--            return $ Left e
+--        Right r -> do 
+--            exitCode <- waitForProcess pHandle 
+--            return $ Right (r,exitCode)  
+--
+--example1 =  terminateOnError 
+--          . feed undefined undefined     
+--          . consume undefined undefined undefined 
+--          . noNothingHandles
+--
 --example2 =  terminateOnError 
 --          . feed undefined undefined     
 --          . consumeCombined undefined undefined 
