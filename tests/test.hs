d6dd3ac8951013e5eec212ccca25208b6d4c17d6
ddc <diaz_carrete@yahoo.com>
2016-01-24 20:15:09 +0100

    reconstructing tests

--- tests/test.hs
+++ tests/test.hs
@@ -40,24 +40,23 @@ import System.Directory
 import System.Process.Streaming
 import Pipes.Transduce 
 import Pipes.Transduce.Text 
+import qualified Pipes.Transduce.Text as PT
 import Pipes.Transduce.ByteString 
 
+main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
 tests = testGroup "Tests" 
             [ testCollectStdoutStderrAsByteString
             , testFeedStdinCollectStdoutAsText  
---            , testCombinedStdoutStderr
---            , testInterruptExecution 
---            , testFailIfAnythingShowsInStderr 
---            , testTwoTextParsersInParallel  
---            , testCountWords 
---            , testSingletonPipeline 
---            , testBasicPipeline
---            , testBranchingPipeline 
---            , testDrainageDeadlock
---            , testAlternatingWithCombined 
+            , testCombinedStdoutStderr
+            , testInterruptExecution 
+            , testFailIfAnythingShowsInStderr 
+            , testTwoTextParsersInParallel  
+            , testCountWords 
+            , testDrainageDeadlock
+            , testAlternatingWithCombined 
 --            , testDecodeFailure
             ]
 
@@ -90,242 +89,160 @@ feedStdinCollectStdoutAsText =
     (pipedShell "cat")
     (feedUtf8 (Just "aaaaaa\naaaaa") *> fold1Out (transduce1 utf8x intoLazyText))
 
--- -------------------------------------------------------------------------------
--- 
--- testCombinedStdoutStderr :: TestTree
--- testCombinedStdoutStderr = testCase "testCombinedStdoutStderr"  $ do
---     r <- combinedStdoutStderr 
---     case r of 
---         (ExitSuccess,TL.lines -> ls) -> do
---             assertEqual "line count" (Prelude.length ls) 4
---             assertBool "expected lines" $ 
---                 getAll $ foldMap (All . flip Prelude.elem ls) $
---                     [ "ooo"
---                     , "ppp"
---                     , "errprefix: eee"
---                     , "errprefix: ffff"
---                     ]
---         _ -> assertFailure "oops"
--- 
--- combinedStdoutStderr :: IO (ExitCode,TL.Text)
--- combinedStdoutStderr = execute
---     (pipeoec (toLines T.decodeIso8859_1 (pure id))
---              (tweakLines annotate $ toLines T.decodeIso8859_1 (pure id))    
---              (withFold T.toLazyM))
---     (pipedShell "{ echo ooo ; echo eee 1>&2 ; echo ppp ;  echo ffff 1>&2 ; }")
---   where
---     annotate x = P.yield "errprefix: " *> x  
--- 
--- -------------------------------------------------------------------------------
--- 
--- testInterruptExecution :: TestTree
--- testInterruptExecution = localOption (mkTimeout $ 5*(10^6)) $
---     testCase "interruptExecution" $ do
---         r <- interruptExecution
---         case r of
---             Left "interrupted" -> return ()
---             _ -> assertFailure "oops"
--- 
--- interruptExecution :: IO (Either String (ExitCode,()))
--- interruptExecution = executeFallibly
---     (pipeo . siphon $ \_ -> runExceptT . throwE $ "interrupted")
---     (pipedShell "sleep 100s")
--- 
--- -------------------------------------------------------------------------------
--- 
--- testFailIfAnythingShowsInStderr :: TestTree
--- testFailIfAnythingShowsInStderr = localOption (mkTimeout $ 5*(10^6)) $
---     testCase "failIfAnythingShowsInStderr" $ do
---         r <- failIfAnythingShowsInStderr 
---         case r of
---             Left "morestuff\n" -> return ()
---             _ -> assertFailure "oops"
--- 
--- failIfAnythingShowsInStderr :: IO (Either T.ByteString (ExitCode,()))
--- failIfAnythingShowsInStderr = executeFallibly
---     (pipee (unwanted ()))
---     (pipedShell "{ echo morestuff 1>&2 ; sleep 100s ; }")
--- 
--- -------------------------------------------------------------------------------
--- 
--- testTwoTextParsersInParallel  :: TestTree
--- testTwoTextParsersInParallel  = testCase "twoTextParsersInParallel" $ do
---     r <- twoTextParsersInParallel
---     case r of 
---         Right (ExitSuccess,("ooooooo","aaaaaa")) -> return ()
---         _ -> assertFailure "oops"
--- 
--- parseChars :: Char -> A.Parser [Char] 
--- parseChars c = fmap mconcat $ 
---     many (A.notChar c) *> A.many1 (some (A.char c) <* many (A.notChar c))
---         
--- parser1 = parseChars 'o'
--- 
--- parser2 = parseChars 'a'
--- 
--- twoTextParsersInParallel :: IO (Either String (ExitCode,([Char], [Char])))
--- twoTextParsersInParallel = executeFallibly
---     (pipeo (encoded T.decodeIso8859_1 (pure id) $ 
---                 (,) <$> adapt parser1 <*> adapt parser2))
---     (pipedShell "{ echo ooaaoo ; echo aaooaoa; }")
---   where
---     adapt p = fromParser $ do
---         r <- P.parse p
---         return $ case r of
---             Just (Right r') -> Right r'
---             _ -> Left "parse error"
--- 
--- -------------------------------------------------------------------------------
--- 
--- testCountWords :: TestTree
--- testCountWords = testCase "testCountWords" $ do
---     r <- countWords 
---     case r of 
---         (ExitSuccess,3) -> return ()                   
---         _ -> assertFailure "oops"
--- 
--- countWords :: IO (ExitCode,Int)
--- countWords = execute
---     (pipeo (encoded T.decodeIso8859_1 (pure id) $
---                 withFold $ P.sum . G.folds const () (const 1) . view T.words))
---     (pipedShell "{ echo aaa ; echo bbb ; echo ccc ; }")
--- 
--- -------------------------------------------------------------------------------
--- testSingletonPipeline :: TestTree
--- testSingletonPipeline = testCase "singletonPipeline" $ do
---     r <- singletonPipeline 
---     case r of
---         (Right ("ooo\nppp\n","eee\nffff\n")) -> return ()
---         _ -> assertFailure "oops"
--- 
--- singletonPipeline :: IO (Either Int (BL.ByteString,BL.ByteString))
--- singletonPipeline =  executePipelineFallibly 
---     (pipeoe (withFold B.toLazyM) 
---             (withFold B.toLazyM)) 
---     (pure $ stage (toLines T.decodeUtf8 (pure id)) pipefail $ 
---          pipedShell "{ echo ooo ; echo eee 1>&2 ; echo ppp ;  echo ffff 1>&2 ; }"
---     )     
--- 
--- -------------------------------------------------------------------------------
--- testBasicPipeline :: TestTree
--- testBasicPipeline = testCase "basicPipeline" $ do
---     r <- basicPipeline 
---     case r of 
---         Right ((),"aaaccc\n") -> return ()                   
---         _ -> assertFailure "oops"
--- 
--- basicPipeline :: IO (Either Int ((),BL.ByteString))
--- basicPipeline =  executePipelineFallibly 
---     (pipeio (fromProducer $ yield "aaabbb\naaaccc\nxxxccc") 
---             (withFold B.toLazyM)) 
---     (fmap (stage (toLines T.decodeUtf8 (pure id)) pipefail) $   
---         Node (pipedShell "grep aaa") [Node (pipedShell "grep ccc") []] )
--- 
--- -------------------------------------------------------------------------------
--- 
--- testBranchingPipeline :: TestTree
--- testBranchingPipeline = testCase "branchingPipeline" $ do
---     exists <- doesFileExist branchingPipelineFile
---     when exists $ removeFile branchingPipelineFile
---     r <- branchingPipeline 
---     case r of 
---         (Right ("ppp\v","eee\nffff\n")) -> return ()                   
---         _ -> assertFailure "oops"
---     fileContents <- withFile branchingPipelineFile ReadMode  $ \hIn -> do
---         B.toLazyM $ B.fromHandle hIn 
---     assertBool "file contexts" $ BL.isPrefixOf "yyy" fileContents 
--- 
--- branchingPipelineFile :: String 
--- branchingPipelineFile = "dist/test/process-streaming-pipeline-text.txt"
--- 
--- branchingPipeline :: IO (Either Int (BL.ByteString, BL.ByteString))
--- branchingPipeline = executePipelineFallibly
---     (pipeoe (withFold B.toLazyM) (withFold B.toLazyM)) 
---     (Node rootStage 
---         [ Node branch1 [Node terminalStage1 []]
---         , Node branch2 [Node terminalStage2 []]
---         ] 
---     )
---   where
---     succStage = P.map (Data.ByteString.map succ)
--- 
---     rootStage :: Stage Int 
---     rootStage = stage (toLines T.decodeIso8859_1 (pure id))                 
---                       pipefail
---                       (pipedShell "{ echo oooaaa ; echo eee 1>&2 ; echo xxx ;  echo ffff 1>&2 ; }")
--- 
---     branch1 :: Stage Int 
---     branch1 = stage (toLines T.decodeIso8859_1 (pure id))                 
---                     pipefail
---                     (pipedShell "grep ooo")
---     branch2 :: Stage Int 
---     branch2 = stage (toLines T.decodeIso8859_1 (pure id))                 
---                     pipefail
---                     (pipedShell "grep xxx")
--- 
---     terminalStage1 :: Stage Int 
---     terminalStage1 = inbound (\p -> p >-> succStage) $
---         stage (toLines T.decodeIso8859_1 (pure id))                 
---               pipefail
---               (pipedShell "tr -d b")
--- 
---     terminalStage2 :: Stage Int
---     terminalStage2 = inbound (\p -> p >-> succStage) $
---         stage (toLines T.decodeIso8859_1 (pure id))                 
---               pipefail
---               (pipedShell $ "cat > " ++ branchingPipelineFile)
--- 
--- -------------------------------------------------------------------------------
--- 
--- testDrainageDeadlock :: TestTree
--- testDrainageDeadlock = localOption (mkTimeout $ 20*(10^6)) $
---     testCase "drainageDeadlock" $ do
---         execute nopiping $ pipedShell "chmod u+x tests/alternating.sh"
---         r <- drainageDeadlock
---         case r of
---             (ExitSuccess,((),())) -> return ()
---             _ -> assertFailure "oops"
--- 
--- -- A bug caused some streams not to be drained, and this caused problems
--- -- due to full output buffers.
--- drainageDeadlock :: IO (ExitCode,((),()))
--- drainageDeadlock = execute
---     (pipeoe (pure ()) (withFold $ \producer -> next producer >> pure ()))
---     (pipedProc "tests/alternating.sh" [])
--- 
--- 
--- -------------------------------------------------------------------------------
--- 
--- testAlternatingWithCombined :: TestTree
--- testAlternatingWithCombined = localOption (mkTimeout $ 20*(10^6)) $
---     testCase "testAlternatingWithCombined" $ do
---         execute nopiping $ pipedShell "chmod u+x tests/alternating.sh"
---         r <- alternatingWithCombined  
---         case r of 
---             (ExitSuccess,80000) -> return ()
---             _ -> assertFailure "oops"
---         r <- alternatingWithCombined2  
---         case r of 
---             (ExitSuccess,(80000,80000)) -> return ()
---             _ -> assertFailure "oops"
--- 
--- alternatingWithCombined :: IO (ExitCode,Integer)
--- alternatingWithCombined = execute
---     (pipeoec lp lp countLines)
---     (pipedProc "tests/alternating.sh" [])
---   where
---     lp = toLines T.decodeIso8859_1 (pure id) 
---     countLines = withFold $ P.sum . G.folds const () (const 1) . view T.lines
--- 
--- 
--- alternatingWithCombined2 :: IO (ExitCode,(Integer,Integer))
--- alternatingWithCombined2 = execute
---     (pipeoec lp lp $ (,) <$> countLines <*> countLines)
---     (pipedProc "tests/alternating.sh" [])
---   where
---     lp = toLines T.decodeIso8859_1 (pure id) 
---     countLines = withFold $ P.sum . G.folds const () (const 1) . view T.lines
--- 
+-------------------------------------------------------------------------------
+
+testCombinedStdoutStderr :: TestTree
+testCombinedStdoutStderr = testCase "testCombinedStdoutStderr"  $ do
+    r <- combinedStdoutStderr 
+    case r of 
+        (TL.lines -> ls) -> do
+            assertEqual "line count" (Prelude.length ls) 4
+            assertBool "expected lines" $ 
+                getAll $ foldMap (All . flip Prelude.elem ls) $
+                    [ "ooo"
+                    , "ppp"
+                    , "errprefix: eee"
+                    , "errprefix: ffff"
+                    ]
+
+combinedStdoutStderr :: IO TL.Text
+combinedStdoutStderr = execute
+    (pipedShell "{ echo ooo ; echo eee 1>&2 ; echo ppp ;  echo ffff 1>&2 ; }")
+    (fold2OutErr (combined (PT.lines utf8x) (groups annotate . PT.lines $ utf8x) intoLazyText))
+  where
+    annotate x = P.yield "errprefix: " *> x  
+
+-------------------------------------------------------------------------------
+
+testInterruptExecution :: TestTree
+testInterruptExecution = localOption (mkTimeout $ 5*(10^6)) $
+    testCase "interruptExecution" $ do
+        r <- interruptExecution
+        case r of
+            Left "interrupted" -> return ()
+            _ -> assertFailure "oops"
+
+interruptExecution :: IO (Either String ())
+interruptExecution = executeFallibly
+    (pipedShell "sleep 100s")
+    (fold1Out $ withFallibleCont $ \_ -> runExceptT . throwE $ "interrupted")
+
+-------------------------------------------------------------------------------
+
+testFailIfAnythingShowsInStderr :: TestTree
+testFailIfAnythingShowsInStderr = localOption (mkTimeout $ 5*(10^6)) $
+    testCase "failIfAnythingShowsInStderr" $ do
+        r <- failIfAnythingShowsInStderr 
+        case r of
+            Left "morestuff\n" -> return ()
+            _ -> assertFailure "oops"
+
+failIfAnythingShowsInStderr :: IO (Either T.ByteString ())
+failIfAnythingShowsInStderr = executeFallibly
+    (pipedShell "{ echo morestuff 1>&2 ; sleep 100s ; }")
+    (fold1Err trip)
+
+-------------------------------------------------------------------------------
+
+testTwoTextParsersInParallel  :: TestTree
+testTwoTextParsersInParallel  = testCase "twoTextParsersInParallel" $ do
+    r <- twoTextParsersInParallel
+    case r of 
+        Right ("ooooooo","aaaaaa") -> return ()
+        _ -> assertFailure "oops"
+
+parseChars :: Char -> A.Parser [Char] 
+parseChars c = fmap mconcat $ 
+    many (A.notChar c) *> A.many1 (some (A.char c) <* many (A.notChar c))
+        
+parser1 :: A.Parser [Char]
+parser1 = parseChars 'o'
+
+parser2 :: A.Parser [Char]
+parser2 = parseChars 'a'
+
+twoTextParsersInParallel :: IO (Either String ([Char], [Char]))
+twoTextParsersInParallel = 
+    executeFallibly
+    (pipedShell "{ echo ooaaoo ; echo aaooaoa; }")
+    (fold1Out (transduce1 utf8x $ 
+                (,) <$> adapt parser1 <*> adapt parser2))
+  where
+    adapt p = withParser $ do
+        r <- P.parse p
+        return $ case r of
+            Just (Right r') -> Right r'
+            _ -> Left "parse error"
+ 
+-------------------------------------------------------------------------------
+
+testCountWords :: TestTree
+testCountWords = testCase "testCountWords" $ do
+    r <- countWords 
+    case r of 
+        3 -> return ()                   
+        _ -> assertFailure "oops"
+
+countWords :: IO Int
+countWords = 
+    execute
+    (pipedShell "{ echo aaa ; echo bbb ; echo ccc ; }")
+    (fold1Out (transduce1 PT.utf8x $
+                withCont $ P.sum . G.folds const () (const 1) . view T.words))
+
+-------------------------------------------------------------------------------
+
+testDrainageDeadlock :: TestTree
+testDrainageDeadlock = localOption (mkTimeout $ 20*(10^6)) $
+    testCase "drainageDeadlock" $ do
+        execute (pipedShell "chmod u+x tests/alternating.sh") (pure ())
+        r <- drainageDeadlock
+        case r of
+            ExitSuccess -> return ()
+            _ -> assertFailure "oops"
+
+-- A bug caused some streams not to be drained, and this caused problems
+-- due to full output buffers.
+drainageDeadlock :: IO ExitCode
+drainageDeadlock = 
+    execute
+    (pipedProc "tests/alternating.sh" [])
+    (fold1Err (withCont $ \producer -> next producer >> pure ()) *> exitCode)
+
+-------------------------------------------------------------------------------
+
+testAlternatingWithCombined :: TestTree
+testAlternatingWithCombined = localOption (mkTimeout $ 20*(10^6)) $
+    testCase "testAlternatingWithCombined" $ do
+        execute (pipedShell "chmod u+x tests/alternating.sh") (pure ())
+        r <- alternatingWithCombined  
+        case r of 
+            80000 -> return ()
+            _ -> assertFailure $ "unexpected lines (1) " ++ show r
+        r <- alternatingWithCombined2  
+        case r of 
+            (80000,80000) -> return ()
+            _ -> assertFailure $ "unexpected lines (2) " ++ show r
+
+alternatingWithCombined :: IO Integer
+alternatingWithCombined = 
+    execute
+    (pipedProc "tests/alternating.sh" [])
+    (fold2OutErr (combined lp lp countLines))
+  where
+    lp = PT.lines PT.utf8x
+    countLines = withCont $ P.sum . G.folds const () (const 1) . view T.lines
+
+
+alternatingWithCombined2 :: IO (Integer,Integer)
+alternatingWithCombined2 = 
+    execute
+    (pipedProc "tests/alternating.sh" [])
+    (fold2OutErr (combined lp lp (liftA2 (,) countLines countLines)))
+  where
+    lp = PT.lines PT.utf8x
+    countLines = withCont $ P.sum . G.folds const () (const 1) . view T.lines
+
 -- -------------------------------------------------------------------------------
 -- 
 -- testDecodeFailure :: TestTree
